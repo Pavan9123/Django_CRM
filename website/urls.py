bac0f9e2353f5ee741d714886012a2e140c80175
Pavan9123 <pavankumar.personal.acc@gmail.com>
2023-08-15 12:21:35 +0530

    new changes - 15/08/23

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from views import *
+from . import views
 
 urlpatterns = [
-    path('', home, name = 'home'),
+    path('', views.home, name = 'home'),
+    #path('login-user/', views.login_user, name = 'login'),
+    #path('logout-user/', views.logout_user, name = 'logout'),
 ]
