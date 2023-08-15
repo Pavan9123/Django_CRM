91f362d20c6696332c399b28a0149f733a2ddc06
Pavan9123 <pavankumar.personal.acc@gmail.com>
2023-08-15 12:16:23 +0530

    new changes

--- website/views.py
+++ website/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, )
+    return render(request, 'home.html',{})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
