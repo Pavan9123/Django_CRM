6e554de1e2cb40a854da1298254f1b8384bfe2c1
Pavan9123 <pavankumar.personal.acc@gmail.com>
2023-08-16 21:06:44 +0530

    add new files

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForms
 
 # Create your views here.
 def home(request):
@@ -27,4 +28,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    pass
+    if request.method == 'POST':
+        form = SignUpForms(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request,user)
+            messages.success(request, "You have been registered successfully.. Welcome!")
+            return redirect('home')
+        
+    else:
+        form = SignUpForms()
+        return render(request, 'register.html',{'form':form})
+
+    return render(request, 'register.html',{'form':form})
