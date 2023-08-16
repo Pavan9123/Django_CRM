5f44c320f638a7f7d29e9ecc0d5eeb01c5e17ffb
Pavan9123 <pavankumar.personal.acc@gmail.com>
2023-08-16 11:33:20 +0530

    changes - 16/08/23

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     #path('login-user/', views.login_user, name = 'login'),
     path('logout-user/', views.logout_user, name = 'logout'),
+    path('register-user', views.register_user, name = 'register'),
 ]
